81cf08c3faecfb0d7c9ff872e660eed3866d5d9f
iamkeerthisdpt <160541883+iamkeerthisdpt@users.noreply.github.com>
2024-07-14 17:25:30 -0700

    Update MyClassTest.java
    import

--- src/test/java/MyClassTest.java
+++ src/test/java/MyClassTest.java
@@ -1,6 +1,7 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
